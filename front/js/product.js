425bfefd4e08e88a4169fb87b3c425bb4807b00b
Niabag <Gabain@hotmail.fr>
2022-08-07 16:28:04 +0200

    Ajout des couleurs et quantity au local storage

--- front/js/product.js
+++ front/js/product.js
@@ -3,6 +3,7 @@ const title = document.getElementById("title")
 const description = document.getElementById("description");
 const price = document.getElementById("price");
 const colors = document.getElementById("colors");
+
 //
 const url = new URLSearchParams(window.location.search)
 const id = url.get("id")
@@ -44,10 +45,22 @@ const product = async () => {
     });
 
 //-------------Local storage------------------------
+    const quantity = document.getElementById("quantity")
+    const button = document.getElementById("addToCart")
+
     const titleRusult = title.innerHTML
     const imgResult = img.getElementsByTagName('img')[0].getAttribute("src")
-    const button = document.getElementById("addToCart")
+    let colorResult = 
+    colors.addEventListener('change', function() {
+        var value = colors.options[colors.selectedIndex].value;
+        colorResult = value;
+      })
+    let quantityResult = 
+    quantity.addEventListener('change', function() {
+        quantityResult = quantity.value
+      })
     const descriptionRusult = description.innerHTML
+
 
     function saveProduct(product){
         localStorage.setItem("product", JSON.stringify(product));
@@ -64,13 +77,14 @@ const product = async () => {
     
     function addProduct(kanap){
         let product = getProduct();
+   
         product.push(kanap);
         saveProduct(product);
     }
     
     function valitation(){
         button.addEventListener('click',() => {
-            addProduct({id,titleRusult,imgResult,descriptionRusult})
+            addProduct({id,titleRusult,imgResult,descriptionRusult,colorResult,quantityResult})
         }) 
     }
     valitation()
